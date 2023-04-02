a22ffba6cee1704d49ed24212f2a0fc39b7463b9
Ed Laptop <ejagombar@gmail.com>
2023-04-02 19:09:20 +0100

    created new test for distance sensor

---
+++ Test Programs/DistanceSensorTest/src/main.cpp
@@ -0,0 +1,109 @@
+#include <Arduino.h>
+
+#include <Wire.h>
+
+#include "SHTSensor.h"
+
+#include "Adafruit_VL53L0X.h"
+
+#include "MS5837.h"
+
+MS5837 sensor;
+// To use a specific sensor instead of probing the bus use this command:
+SHTSensor sht(SHTSensor::SHTC3);
+
+Adafruit_VL53L0X lox = Adafruit_VL53L0X();
+
+void setup() {
+  // put your setup code here, to run once:
+
+  pinMode(D10, OUTPUT);
+  digitalWrite(D10, HIGH);
+
+    pinMode(D12, OUTPUT);
+  digitalWrite(D12, HIGH);
+
+      pinMode(D11, OUTPUT);
+  digitalWrite(D11, HIGH);
+
+        pinMode(D7, OUTPUT);
+  digitalWrite(D7, HIGH);
+
+  Wire.begin();
+  Serial.begin(115200);
+  delay(1000); // let serial console settle
+
+  if (sht.init()) {
+      Serial.print("SHTC3 boot : Success\n");
+  } 
+  else {
+      Serial.print("SHTC3 boot : Failed\n");
+  }
+  sht.setAccuracy(SHTSensor::SHT_ACCURACY_MEDIUM); // only supported by SHT3x
+
+  Serial.println("Adafruit VL53L0X test");
+  if (!lox.begin()) {
+    Serial.println(F("Failed to boot VL53L0X"));
+    while(1);
+  }
+  // power 
+  Serial.println(F("VL53L0X API Simple Ranging example\n\n")); 
+
+  while (!sensor.init()) {
+    Serial.println("Init failed!");
+    Serial.println("Are SDA/SCL connected correctly?");
+    Serial.println("Blue Robotics Bar30: White=SDA, Green=SCL");
+    Serial.println("\n\n\n");
+    delay(5000);
+  }
+
+  sensor.setModel(MS5837::MS5837_02BA);
+  sensor.setFluidDensity(997); // kg/m^3 (freshwater, 1029 for seawater)
+
+}
+
+const int SampleSize = 25;  // define the sample size
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  VL53L0X_RangingMeasurementData_t measure;
+  float accumulator = 0;
+
+  for(int i = 0; i < SampleSize; i++)  // take the sum of multiple readings
+  {
+    lox.rangingTest(&measure, false); // pass in 'true' to get debug data printout!
+
+    if (measure.RangeStatus != 4) // keep only the good readings
+    {  
+      accumulator += lox.readRange();
+    }
+  }
+
+  Serial.print("Height: ");
+  Serial.print(accumulator / SampleSize);  // print average of all samples
+  Serial.print(" mm\n");
+
+  if (sht.readSample()) {
+      Serial.print("Humidity: ");
+      Serial.print(sht.getHumidity(), 2);
+      Serial.print("\nTemperature (°C): ");
+      Serial.print(sht.getTemperature(), 2);
+      Serial.print("\n");
+  } else {
+      Serial.print("Error in readSample()\n");
+  }
+
+  sensor.read();
+
+  Serial.print("Pressure: "); 
+  Serial.print(sensor.pressure()); 
+  Serial.println(" mbar");
+
+  Serial.print("Altitude: "); 
+  Serial.print(sensor.altitude()); 
+  Serial.println(" m above mean sea level\n");
+
+
+  delay(50);
+
+}
