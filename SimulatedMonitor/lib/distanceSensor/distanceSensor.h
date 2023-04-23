f7be70f02ed08ddeff34844753d0f58bf6534b08
Chip <ejagombar@gmail.com>
2023-04-23 22:17:00 +0100

    updated header file for simulation too

--- SimulatedMonitor/lib/distanceSensor/distanceSensor.h
+++ SimulatedMonitor/lib/distanceSensor/distanceSensor.h
@@ -2,16 +2,12 @@
 #define DISTANCESENSOR_H
 
 #include <Arduino.h>
-#include <Wire.h>
 
-#include "Adafruit_VL53L0X.h"
 #include "sampleBuffer.h"
 #include "Error.h"
 
 class DistanceSensor {
    private:
-    Adafruit_VL53L0X sensor;
-    VL53L0X_RangingMeasurementData_t measureData;
     const int SampleSize = 6;
     int result;
 
