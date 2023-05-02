74ab2ece5588cd4194df6714a9e37237fa29c980
Ed Laptop <ejagombar@gmail.com>
2023-05-02 14:09:00 +0100

    created test program

--- Test Programs/PressureSensorCalibration/src/main.cpp
+++ Test Programs/PressureSensorCalibration/src/main.cpp
@@ -1,9 +1,27 @@
 #include <Arduino.h>
 
+#include "pressureSensor.h"
+
+#define PressureLidarVcc1 D11
+#define PressureLidarVcc2 D12
+
+PressureSensor pressureSensor;
+
 void setup() {
-  // put your setup code here, to run once:
+    Serial.begin(115200);
+
+    pinMode(PressureLidarVcc1, OUTPUT);
+    pinMode(PressureLidarVcc2, OUTPUT);
+
+    digitalWrite(PressureLidarVcc1, HIGH);
+    digitalWrite(PressureLidarVcc2, HIGH);
+
+    pressureSensor.setup();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    if (pressureSensor.measure() != NO_ERROR) {
+        Serial.println("Error occured");
+    }
+    Serial.println(pressureSensor.getDepth());
 }
